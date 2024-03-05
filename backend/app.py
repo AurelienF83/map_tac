e5e5c808d943f267966a96e8ed42534be90ba8a7
A.Francesch <franceschaurelien@gmail.com>
2024-03-05 17:40:39 +0100

    Gestion jsonx,y et nom Parc. Fonction offset si x,y identique

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,37 @@
 from flask import Flask, jsonify
 import pandas as pd
-from flask_cors import CORS  # Import CORS
+from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
+CORS(app)
 
 @app.route('/locations', methods=['GET'])
 def get_locations():
-    # Lire le fichier Excel et convertir en JSON
-    df = pd.read_excel('G:\\DL\\ps2.xlsx')
-    locations = df['geo_point_2d'].str.split(',', expand=True).values.tolist()
+    # Lire le fichier Excel
+    df = pd.read_excel('C:\\Users\\a.francesch\\Desktop\\Caroline\\Mise en service_TAC_20240304.xlsx')
+
+    # Nettoyer le DataFrame en supprimant les lignes avec des NaN dans 'Adresse Parc' ou 'Nom du parc'
+    df = df.dropna(subset=['Adresse Parc', 'Nom du parc'])
+
+    # Diviser 'Adresse Parc' en deux nouvelles colonnes 'lat' et 'lng'
+    coords = df['Adresse Parc'].str.split(',', expand=True)
+    df['lat'] = pd.to_numeric(coords[0], errors='coerce')
+    df['lng'] = pd.to_numeric(coords[1], errors='coerce')
+
+    # Assigner la colonne 'Nom du parc' à 'name'
+    df['name'] = df['Nom du parc']
+
+    # Supprimer à nouveau les lignes où 'lat' ou 'lng' sont NaN après la conversion
+    df = df.dropna(subset=['lat', 'lng'])
+
+    # Créer une liste de dictionnaires pour la conversion en JSON
+    locations = df[['lat', 'lng', 'name']].to_dict(orient='records')
+
     return jsonify(locations)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
 
 
