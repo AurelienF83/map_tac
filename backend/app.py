ee5393e36c5145355d686fc1713375b3af80cfc6
AurelienF83 <a-francesch@hotmail.fr>
2024-03-06 12:56:44 +0100

    QoL

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ def get_locations():
     # Lire le fichier Excel
     df = pd.read_excel('C:\\Users\\a.francesch\\Desktop\\Caroline\\Mise en service_TAC_20240304.xlsx')
 
-    # Nettoyer le DataFrame en supprimant les lignes avec des NaN dans 'Adresse Parc' ou 'Nom du parc'
+    # Nettoyer le DataFrame en supprimant les lignes avec des NaN dans 'les colonnes du xlsx
     df = df.dropna(subset=['Adresse Parc', 'Nom du parc', 'Date', 'Adresse PS'])
 
     # Diviser 'Adresse Parc' en deux nouvelles colonnes 'lat' et 'lng'
@@ -18,7 +18,7 @@ def get_locations():
     df['lat'] = pd.to_numeric(coords[0], errors='coerce')
     df['lng'] = pd.to_numeric(coords[1], errors='coerce')
 
-    # Assigner la colonne 'Nom du parc' à 'name'
+    # Assigner les colonnes df à celle du xlsx
     df['name'] = df['Nom du parc']
     df['date'] = df['Date'].dt.strftime('%d/%m/%Y')
     df['ps'] = df ['Adresse PS']
@@ -26,7 +26,7 @@ def get_locations():
     # Supprimer à nouveau les lignes où 'lat' ou 'lng' sont NaN après la conversion
     df = df.dropna(subset=['lat', 'lng'])
 
-    # Créer une liste de dictionnaires pour la conversion en JSON
+    # Liste retournée au JSON
     locations = df[['lat', 'lng', 'name', 'date', 'ps']].to_dict(orient='records')
 
     return jsonify(locations)
